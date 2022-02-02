b4ecdb9759b6abe7b08dde60452d0e70f0b4432f
Luke Cordova <lukethegreat000@gmail.com>
2022-02-01 21:25:29 -0800

    testing out pyautogui functionality

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import time
+import threading
 import pyautogui as ptg
 import keyboard as key
 
@@ -11,3 +12,66 @@ import keyboard as key
 #     print(ptg.position())
 #     if key.is_pressed('q'): 
 #         quit()
+
+
+
+
+def click():
+    pos = ptg.position()
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+    ptg.click(pos.x, pos.y)
+
+clicks = 0
+c = False
+while 1 :
+    if key.is_pressed('t'):
+        if c:
+            c = False
+            print(f'not clicking clicked: {clicks}')
+            clicks = 0
+            time.sleep(.5)
+        else:
+            c = True
+            print('clicking')
+            time.sleep(.5)
+    if c:
+        # pos = ptg.position()
+        # ptg.click(pos.x, pos.y)
+        threads = []
+        for _ in range(50):
+            t = threading.Thread(target=click)
+            t.start()
+            threads.append(t)
+            clicks += 40
+        for thread in threads:
+            thread.join()
+    if key.is_pressed('q'):
+        quit()
